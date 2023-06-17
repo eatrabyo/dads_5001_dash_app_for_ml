21a5be7ecae8a328711c5c4adab3c6675bd038dc
mmaiip <nattasorn.mai@gmail.com>
2023-06-17 20:12:40 +0700

    f

--- classification_report_heat.py
+++ classification_report_heat.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import pandas as pd
+import plotly.express as px
 
 
 def get_classification_report(y_test, y_pred):
@@ -11,6 +12,14 @@ def get_classification_report(y_test, y_pred):
 def classification_fig(y_test, test_yhat):
     report_df = get_classification_report(y_test, test_yhat)
     report_df = report_df.iloc[0:10, 0:3]
-    class_fig = sns.heatmap(report_df, annot=True, fmt=".2f")
-    return class_fig , report_df
+    report_df = report_df.round(2)
+    class_fig = px.imshow(report_df, x= report_df.columns, y = report_df.index, 
+                          width=50,
+                          color_continuous_scale='YlGnBu_r',text_auto=True)
+    
+    class_fig.update_traces(hovertemplate='digit: %{x}</> Value:%{z:.2f}</>Score:%{y}')
+    class_fig.update_xaxes(side="top")
+    class_fig.update_layout(showlegend = False)
+    
+    return class_fig 
 
